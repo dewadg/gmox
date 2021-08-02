7326f6d794e936f2e778f85b12d20bd7d87eb1d6
Adhyaksa Jaya <dewadg@dewadg.id>
2021-08-02 19:38:04 +0700

    feat: initial sidebar template

---
+++ src/components/GAlphabetIcon.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="g-alphabet-icon">
+    <span>
+      <slot />
+    </span>
+  </div>
+</template>
+
+<style lang="scss">
+@import '../assets/styles/variables.scss';
+
+.g-alphabet-icon {
+  display: flex;
+  width: 20px;
+  height: 20px;
+  align-items: center;
+  justify-content: center;
+  background: $accent;
+  border-radius: 3px;
+
+  span {
+    font-size: 0.8em;
+  }
+}
+</style>
--- src/components/GSidebar.vue
+++ src/components/GSidebar.vue
@@ -1,20 +1,73 @@
 <template>
   <div class="g-sidebar">
-    <ul class="proto-list">
-      <li class="proto-list-item">
-        <div>greeter.proto</div>
-        <span>Test</span>
+    <ul class="g-navbar-list">
+      <li class="g-navbar-list-item">
+        <div class="g-navbar-list-item-container">
+          <div class="g-navbar-list-item-title">
+            <GAlphabetIcon>P</GAlphabetIcon>
+            greeter.proto
+          </div>
+          <span>Test</span>
+        </div>
+
+        <ul class="g-navbar-list second">
+          <li class="g-navbar-list-item">
+            <div class="g-navbar-list-item-container">
+              <div class="g-navbar-list-item-title">
+                <GAlphabetIcon>S</GAlphabetIcon>
+                Greeter
+              </div>
+              <span>Test</span>
+            </div>
+          </li>
+          <li class="g-navbar-list-item">
+            <div class="g-navbar-list-item-container">
+              <div class="g-navbar-list-item-title">
+                <GAlphabetIcon>S</GAlphabetIcon>
+                Responder
+              </div>
+              <span>Test</span>
+            </div>
+          </li>
+        </ul>
       </li>
-      <li class="proto-list-item">
-        <div>user.proto</div>
-        <span>Test</span>
+      <li class="g-navbar-list-item">
+        <div class="g-navbar-list-item-container">
+          <div class="g-navbar-list-item-title">
+            <GAlphabetIcon>P</GAlphabetIcon>
+            user.proto
+          </div>
+          <span>Test</span>
+        </div>
       </li>
     </ul>
   </div>
 </template>
 
+<script>
+import GAlphabetIcon from './GAlphabetIcon.vue'
+
+export default {
+  components: {
+    GAlphabetIcon
+  }
+}
+</script>
+
 <style lang="scss">
 @import '../assets/styles/variables.scss';
+
+.g-navbar-list-item-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: $font-secondary;
+
+  &:hover{
+    background: $secondary;
+    color: $font-primary;
+  }
+}
 
 .g-sidebar {
   position: fixed;
@@ -24,17 +77,29 @@
   height: 100%;
   border-right: 1px solid $accent;
 
-  .proto-list {
+  .g-navbar-list {
     list-style: none;
-    margin: 15px 0 0 0;
     padding: 0;
-    color: $font-secondary;
 
-    .proto-list-item {
-      display: flex;
-      justify-content: space-between;
-      padding: 15px;
+    &.second .g-navbar-list-item > .g-navbar-list-item-container {
+      padding: 10px 15px 10px 30px;
+    }
+
+    .g-navbar-list-item {
       font-size: 0.9em;
+
+      .g-navbar-list-item-container {
+        padding: 15px;
+      }
+
+      .g-navbar-list-item-title {
+        display: flex;
+        align-items: center;
+
+        .g-alphabet-icon {
+          margin-right: 10px;
+        }
+      }
     }
   }
 }
