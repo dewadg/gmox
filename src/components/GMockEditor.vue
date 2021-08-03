e835ed9156cb890e0894abfa986a205eb177ed0b
Adhyaksa Jaya <dewadg@dewadg.id>
2021-08-04 01:30:24 +0700

    refactor: adjust GMockEditor layout

--- src/components/GMockEditor.vue
+++ src/components/GMockEditor.vue
@@ -1,15 +1,23 @@
 <template>
-  <div
-    id="editor"
-    class="g-mock-editor"
-  />
+  <div class="g-mock-editor">
+    <div class="g-mock-editor-title">
+      <GAlphabetIcon v-if="methodChosen">M</GAlphabetIcon>
+      <span>{{ title }}</span>
+    </div>
+    <div id="editor" />
+  </div>
 </template>
 
 <script>
 import { onMounted } from 'vue'
 import * as monaco from 'monaco-editor'
+import GAlphabetIcon from './GAlphabetIcon.vue'
 
 export default {
+  components: {
+    GAlphabetIcon
+  },
+
   setup() {
     onMounted(() => {
       monaco.editor.create(document.getElementById('editor'), {
@@ -34,16 +42,42 @@ export default {
         fontSize: '15px'
       })
     })
+
+    const title = 'No service method currently opened. You can import proto in left sidebar'
+    const methodChosen = true
+
+    return {
+      title,
+      methodChosen
+    }
   }
 }
 </script>
 
 <style lang="scss">
+@import '../assets/styles/variables.scss';
+
+#editor {
+  height: 100%;
+}
+
 .g-mock-editor {
   position: fixed;
   top: 76px;
   left: 251px;
   width: calc((100% - 1px - 250px) * 0.6);
   height: calc(100% - 76px);
+  border-right: 1px solid $accent;
+
+  .g-mock-editor-title {
+    display: flex;
+    align-items: center;
+    padding: 15px;
+    border-bottom: 1px solid $accent;
+
+    .g-alphabet-icon {
+      margin-right: 15px;
+    }
+  }
 }
 </style>
